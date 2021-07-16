48b77a99ea7e2cc348b888cc21269a9f4de578b5
changfs <liuyq1@fosun.com>
2021-07-16 16:45:10 +0800

    request_post 写入

---
+++ flask_api.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app=app)
+
+
+class Hello_World(Resource):
+    def get(self):
+        return "get_data"
+
+    def post(self):
+        return "post_data"
+
+
+api.add_resource(Hello_World, "/test")
+
+if __name__ == "__main__":
+    app.run()
--- request_demo.py
+++ request_demo.py
@@ -1,7 +1,10 @@
 import requests
 
+
 url = 'http://127.0.0.1:5000/?name=name'
-
 rep = requests.get(url=url)
-
 print(rep.text)
+
+
+
+
