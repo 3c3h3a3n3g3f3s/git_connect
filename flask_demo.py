6cac9dfccf98a047305104ff3dddbf8853121e25
changfs <liuyq1@fosun.com>
2021-07-16 15:53:55 +0800

    flask_demo.py 写入

--- flask_demo.py
+++ flask_demo.py
@@ -1,7 +1,13 @@
 from flask import Flask, request
 
-app = Flask(__file__)
+app = Flask(__name__)
 
 
+@app.route("/", methods=['get'])
 def get_data():
-    pass
+    name = request.args.get("name")
+    return name
+
+
+if __name__ == "__main__":
+    app.run()
