ce33e8e6e7d611e0d2ac0a7e6fd32442d7519ae5
changfs <liuyq1@fosun.com>
2021-07-19 16:40:03 +0800

    spider_demo 写入

---
+++ spider_dir/spider_demo.py
@@ -0,0 +1,9 @@
+
+
+s_cut = [('72af8ecf3609a546bac3150c20f70455', ['老凤祥', '六福珠宝', '周生生', '亚一珠宝', '亚一金店']),
+              ('3e78397f7dbb88ffbd78ba52d0e925fa', ['老庙', '谢瑞麟', '中国黄金', '明牌珠宝']),  # yh
+              ('6bee32b2f0719ea45cc194847efd8917', ['周大福', '潮宏基', '东华美钻', '周大生']),  # zyy
+              ]
+
+for s_key, store_names in s_cut:
+    print(s_key, store_names)
